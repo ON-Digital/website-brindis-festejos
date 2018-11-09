f5265d2098ea006ea41e57f3a50b087f4abe9d1b
Roberto <robertoeat91@gmail.com>
2018-11-09 12:47:31 -0400

    Reemplazo de archivo footer

--- festejos/festejos/footer.php
+++ festejos/festejos/footer.php
@@ -154,7 +154,7 @@
       </h2>
 
       <div class="whole-cols-width">
-        <!-- <iframe class="mw-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.8344695079504!2d-79.51002868521368!3d8.987374993549734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8faca904597702d9%3A0x9f34060c81637512!2sFestejos+y+Brindis!5e0!3m2!1ses!2sve!4v1541330588977" width="9999" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
+        <iframe class="mw-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.8344695079504!2d-79.51002868521368!3d8.987374993549734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8faca904597702d9%3A0x9f34060c81637512!2sFestejos+y+Brindis!5e0!3m2!1ses!2sve!4v1541330588977" width="9999" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
 
       <?php
